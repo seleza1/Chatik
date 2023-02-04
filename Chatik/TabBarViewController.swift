ee1175f71f69a53ff42ebe851ff5016b95f04e13
seleza1 <koposov.serezha@gmail.com>
2023-02-04 11:05:46 +0300

    create TabBarController

---
+++ Chatik/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  Chatik
+//
+//  Created by user on 04.02.2023.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
