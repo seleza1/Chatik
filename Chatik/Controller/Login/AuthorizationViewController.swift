27e505fbc2077af36b1abec3c59783fc324ebf43
seleza1 <koposov.serezha@gmail.com>
2023-02-05 10:42:49 +0300

    finish layout registerVC and LoginVC

--- Chatik/Controller/Login/AuthorizationViewController.swift
+++ Chatik/Controller/Login/AuthorizationViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AuthenticatorViewController: UIViewController {
+class AuthorizationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
